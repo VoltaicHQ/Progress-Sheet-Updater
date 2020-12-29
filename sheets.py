744d346de595c55463c3f88e1292d73995b58ffc
MingoDynasty <github@mingodynasty.com>
2020-12-28 21:10:06 -0800

    PyCharm auto formatter.

--- sheets.py
+++ sheets.py
@@ -17,10 +17,10 @@ def validate_sheet_range(str):
 def read_sheet_range(api, id, sheet_range):
     try:
         response = (api.values()
-                       .get(spreadsheetId=id, range=sheet_range)
-                       .execute()
-                       .get('values', [['0']]))
-        
+                    .get(spreadsheetId=id, range=sheet_range)
+                    .execute()
+                    .get('values', [['0']]))
+
         # responses trim blank cells, act as if they are 0-filled
         m = validate_sheet_range(sheet_range)
         length = int(m.group('row2')) - int(m.group('row1')) + 1
