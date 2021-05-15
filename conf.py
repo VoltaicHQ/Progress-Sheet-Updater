023eff9a228c5be5736c863ff4ceaa9f1f021ff8
Y0ndaime <45723700+Y0ndaime@users.noreply.github.com>
2021-05-15 20:53:02 +0200

    Fix bug
    When using pyinstaller the path has to be found differently (https://stackoverflow.com/questions/404744/determining-application-path-in-a-python-exe-generated-by-pyinstaller)

--- conf.py
+++ conf.py
@@ -1,8 +1,12 @@
 import os
 import pathlib
+import sys
 
-
-PROJECT_DIR = pathlib.Path(__file__).parent.absolute()
+# determine if application is a script file or frozen exe
+if getattr(sys, 'frozen', False):
+    PROJECT_DIR = pathlib.Path(sys.executable).parent.absolute()
+elif __file__:
+    PROJECT_DIR = pathlib.Path(__file__).parent.absolute()
 LOG_FILE_PATH = os.path.join(PROJECT_DIR, 'logging.conf')
 SPREADSHEET_TOKEN_FILE_PATH = os.path.join(PROJECT_DIR, 'token.pickle')
 SPREADSHEET_CREDENTIALS_FILE_PATH = os.path.join(PROJECT_DIR, 'credentials.json')
